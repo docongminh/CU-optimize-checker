57c51c45fdc3fd001047aca914ef3c2352ba9c82
docongminh <minhdc@192.168.102.71>
2024-12-30 13:36:57 +0700

    init

---
+++ target/debug/deps/cu_check-bc5e084da041f68b.d
@@ -0,0 +1,5 @@
+/Users/minhdc/.personal/CU-optimize-checker/cu-check/target/debug/deps/libcu_check-bc5e084da041f68b.rmeta: src/main.rs
+
+/Users/minhdc/.personal/CU-optimize-checker/cu-check/target/debug/deps/cu_check-bc5e084da041f68b.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/cu_check-d496a04c2a21a1f1.d
@@ -0,0 +1,5 @@
+/Users/minhdc/.personal/CU-optimize-checker/cu-check/target/debug/deps/libcu_check-d496a04c2a21a1f1.rmeta: src/main.rs
+
+/Users/minhdc/.personal/CU-optimize-checker/cu-check/target/debug/deps/cu_check-d496a04c2a21a1f1.d: src/main.rs
+
+src/main.rs:
